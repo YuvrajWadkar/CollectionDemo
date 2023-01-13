d0c7dc7c0714ec3f344ca5d923ea34f93d00cb1b
unknown <yuvrajwadkar24@gmail.com>
2023-01-13 15:48:19 +0530

    File Added Sucesfully

---
+++ src/com/velocity/ArrayListDemo.java
@@ -0,0 +1,21 @@
+package com.velocity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ArrayListDemo {
+	
+	public static void main(String[] args) {
+	
+		List<String> list = new ArrayList<String>();
+		
+		list.add("Solapur");
+		
+		list.add("pune");
+		
+		list.add("hydrabad");
+		
+		System.out.println(list);
+		
+}
+}
